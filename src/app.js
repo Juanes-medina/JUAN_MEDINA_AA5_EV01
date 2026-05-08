a36db87779eb97079e563bc6ff63efb04cefaf3f
Juan Esteban Medina Mendez <jem13medina@gmail.com>
2026-05-07 19:36:51 -0500

    httml

--- src/app.js
+++ src/app.js
@@ -6,7 +6,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/api', authRoutes);
 
 module.exports = app;
+app.get('/', (req, res) => {
+    res.json({
+        mensaje: 'API funcionando'
+    });
+});
